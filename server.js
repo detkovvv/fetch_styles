45159f6e9acd941819d8ffeeb3eb6cb546023dc3
detkov <fgh.detkov@gmail.com>
2024-05-06 00:42:59 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,90 @@
+const http = require('http');
+const fs = require('fs');
+
+// Чтение конфигурационного файла
+const config = JSON.parse(fs.readFileSync('./src/services/api.cfg', 'utf8'));
+
+// Функция для генерации случайного числа в заданном диапазоне
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Объекты для сообщений об ошибках и заголовков CORS
+const errors = {
+    methodNotAllowed: 'Method Not Allowed',
+    notFound: 'Not Found'
+};
+
+const corsHeaders = {
+    origin: '*',
+    methods: 'GET',
+    headers: 'origin, content-type, accept'
+};
+
+// Функция для обработки запроса на /api/button/
+const handleButtonRequest = (req, res) => {
+    if (req.method === 'GET') {
+
+        // Моделируем задержку сервера
+        setTimeout(() => {
+            const forms = ['square', 'rectangle', 'triangle', 'circle'];
+            const colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'violet'];
+
+            // Генерируем случайную форму и цвет
+            const randomForm = forms[getRandomInt(0, forms.length - 1)];
+            const randomColor = colors[getRandomInt(0, colors.length - 1)];
+
+            const responseData = JSON.stringify({form: randomForm, color: randomColor});
+
+            setCorsHeaders(res);
+
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(responseData);
+        }, getRandomInt(1000, 5000)); // Случайная задержка от 1 до 5 секунд
+    } else {
+        res.writeHead(405, {'Content-Type': 'application/json'});
+        res.end(errors.methodNotAllowed);
+    }
+}
+
+// Функция для обработки запроса на /api/random/
+const handleRandomRequest = (req, res) => {
+    if (req.method === 'GET') {
+        // Моделируем задержку сервера
+        setTimeout(() => {
+            const responseData = JSON.stringify({answer: getRandomInt(1, 100)});
+
+            setCorsHeaders(res);
+
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(responseData);
+        }, getRandomInt(1000, 3000)); // Случайная задержка от 1 до 3 секунд
+    } else {
+        res.writeHead(405, {'Content-Type': 'application/json'});
+        res.end(errors.methodNotAllowed);
+    }
+}
+
+//Установка CORS-заголовков
+const setCorsHeaders = (res) => {
+    res.setHeader('Access-Control-Allow-Origin', corsHeaders.origin);
+    res.setHeader('Access-Control-Allow-Methods', corsHeaders.methods);
+    res.setHeader('Access-Control-Allow-Headers', corsHeaders.headers);
+};
+
+// Создание HTTP-сервера
+const server = http.createServer((req, res) => {
+    if (req.url === '/api/button/') {
+        handleButtonRequest(req, res);
+    } else if (req.url === '/api/random/') {
+        handleRandomRequest(req, res);
+    } else {
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end(errors.notFound);
+    }
+});
+
+// Слушаем порт из конфигурации
+server.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+});
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import config from '../src/services/api.cfg';
+
+// Получаем DOM элементы
+const fetchButton = document.getElementById('fetchButton');
+const responseForm = document.getElementById('responseForm');
+const responseColor = document.getElementById('responseColor');
+const randomData = document.getElementById('randomData');
+
+// Присваиваем адрес запроса
+const serverUrl = `${config.server}:${config.port}`;
+
+// Отправка запроса на сервер при нажатии кнопки
+fetchButton.addEventListener('click', () => {
+    // Блокируем кнопку и меняем её цвет на серый
+    fetchButton.disabled = true;
+    fetchButton.style.backgroundColor = 'grey';
+
+    // Отправка запроса на сервер
+    fetch(`${serverUrl}/api/button/`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Обработка полученного ответа
+            responseForm.textContent = data.form;
+            responseColor.textContent = data.color;
+            fetchButton.style.backgroundColor = data.color;
+            fetchButton.className = `button ${data.form}`;
+
+        })
+        .catch(error => {
+            // Вывод ошибки, если запрос не удался
+            console.error('There was a problem with the fetch operation:', error);
+            responseForm.textContent = 'Error';
+            responseColor.textContent = 'Error';
+        })
+        .finally(() => {
+            // Восстанавливаем кнопку после завершения запроса
+            fetchButton.disabled = false;
+        });
+});
+
+// Обновление третьего поля каждую секунду
+setInterval(() => {
+    fetch(`${serverUrl}/api/random/`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Обработка полученного ответа
+            randomData.textContent = data.answer;
+        })
+        .catch(error => {
+            // Вывод ошибки, если запрос не удался
+            console.error('There was a problem with the fetch operation:', error);
+            randomData.textContent = 'Error';
+        });
+}, 1000);
